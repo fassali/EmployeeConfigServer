c6bc9d3037670bb9b2affdbc61fdb361fd0391ad
Shamik Mitra <mitrashamik@gmail.com>
2018-03-11 10:14:58 +0530

    config server

---
+++ src/main/java/com/example/MicroserviceConfigServer/Test.java
@@ -0,0 +1,10 @@
+package com.example.MicroserviceConfigServer;
+
+public class Test {
+	public static void main(String[] args) {
+		String s = "b";
+		boolean b = s.matches("[a-d1-7]");
+		System.out.println(b);
+	}
+
+}
